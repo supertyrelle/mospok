89de2e94028cd7c0f094dcc5ccf09428a45ba74d
Tyrelle Phillips <work@supertyrelle.com>
2019-08-06 21:12:27 -0700

    generated index desktop design using js

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -3597,6 +3597,12 @@ var Glide$1 = function (_Core) {
 
 var _default = Glide$1;
 exports.default = _default;
+},{}],"img/robo1.jpg":[function(require,module,exports) {
+module.exports = "/robo1.063bea95.jpg";
+},{}],"img/robo2.jpg":[function(require,module,exports) {
+module.exports = "/robo2.19617e06.jpg";
+},{}],"img/robo3.jpg":[function(require,module,exports) {
+module.exports = "/robo3.7f02df6c.jpg";
 },{}],"../node_modules/aos/dist/aos.js":[function(require,module,exports) {
 var define;
 var global = arguments[3];
@@ -3680,13 +3686,17 @@ module.exports = reloadCSS;
 
 var _glide = _interopRequireDefault(require("@glidejs/glide"));
 
+var _robo = _interopRequireDefault(require("./img/robo1.jpg"));
+
+var _robo2 = _interopRequireDefault(require("./img/robo2.jpg"));
+
+var _robo3 = _interopRequireDefault(require("./img/robo3.jpg"));
+
 var _aos = _interopRequireDefault(require("aos"));
 
 require("aos/dist/aos.css");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-new _glide.default('.glide').mount();
 
 _aos.default.init({
   // Global settings:
@@ -3715,14 +3725,236 @@ _aos.default.init({
   // values from 0 to 3000, with step 50ms
   easing: 'ease',
   // default easing for AOS animations
-  once: false,
+  once: true,
   // whether animation should happen only once - while scrolling down
-  mirror: false,
+  mirror: true,
   // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
 
-});
-},{"@glidejs/glide":"../node_modules/@glidejs/glide/dist/glide.esm.js","aos":"../node_modules/aos/dist/aos.js","aos/dist/aos.css":"../node_modules/aos/dist/aos.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+}); // const footer = document.querySelector('footer');
+
+
+function headerComp() {
+  // create header alement, assign flexbox class
+  var header = document.querySelector('header');
+  header.setAttribute("class", 'row-sb-c'); // create logo text
+
+  var logo = document.createElement('div');
+  logo.setAttribute("class", 'logo');
+  logo.textContent = "moSpo"; // append the logo to its element
+
+  header.appendChild(logo); // create the main menu element
+
+  function mainNavComp() {
+    var mainNav = document.createElement('nav');
+    header.appendChild(mainNav); // assign main link elements and add to the main nav
+
+    var homeLink = document.createElement('a'),
+        galleryLink = document.createElement('a'),
+        museumLink = document.createElement('a'),
+        ticketsLink = document.createElement('a');
+    homeLink.textContent = "Home";
+    mainNav.appendChild(homeLink);
+    galleryLink.textContent = "Gallery";
+    mainNav.appendChild(galleryLink);
+    museumLink.textContent = "Museum";
+    mainNav.appendChild(museumLink);
+    ticketsLink.textContent = "Buy Tickets";
+    mainNav.appendChild(ticketsLink);
+  }
+
+  mainNavComp(); // create the social media nav element
+
+  function socNavComp() {
+    var socNav = document.createElement('nav');
+    socNav.setAttribute('class', 'row-fe');
+    header.appendChild(socNav); // assign main link elements and add to the main nav
+
+    var dribLink = document.createElement('a'),
+        twitLink = document.createElement('a'),
+        instLink = document.createElement('a');
+    dribLink.setAttribute('class', 'fab fa-dribbble');
+    socNav.appendChild(dribLink);
+    twitLink.setAttribute('class', 'fab fa-twitter');
+    socNav.appendChild(twitLink);
+    instLink.setAttribute('class', 'fab fa-instagram');
+    socNav.appendChild(instLink);
+  }
+
+  socNavComp();
+}
+
+;
+headerComp();
+
+function mainComp() {
+  var app = document.querySelector('main');
+  app.setAttribute("id", 'swup'); // app.setAttribute(`data-aos`, 'fade-up');
+  // create slider container
+
+  var sliderCont = document.createElement('div');
+  sliderCont.setAttribute('class', 'glide');
+  app.appendChild(sliderCont); // create the secondary wrapper for the slider
+
+  var slideTrack = document.createElement('div');
+  slideTrack.setAttribute('class', 'glide__track');
+  slideTrack.setAttribute('data-glide-el', 'track');
+  sliderCont.appendChild(slideTrack); // create the slider slides wrapper
+
+  var slidesCont = document.createElement('ul');
+  slidesCont.setAttribute('class', 'glide__slides');
+  slideTrack.appendChild(slidesCont);
+
+  function slideOneComp() {
+    var slideOne = document.createElement('li');
+    slideOne.setAttribute('class', 'glide__slide row-sb-c');
+    slidesCont.appendChild(slideOne);
+    var content = document.createElement('div'),
+        illustration = document.createElement('div');
+    content.setAttribute('class', 'content');
+    illustration.setAttribute('class', 'illustration');
+    slideOne.appendChild(content);
+    slideOne.appendChild(illustration);
+
+    function contentComp() {
+      var preTitle = document.createElement('h6'),
+          title = document.createElement('h1'),
+          desc = document.createElement('p'),
+          button = document.createElement('button');
+      preTitle.textContent = "February 6 - March 12";
+      title.textContent = "Ive's Robo Exhibit";
+      desc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet\n                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam illo perferendis. Molestias, saepe?";
+      button.textContent = "Learn More";
+      button.setAttribute('class', 'feat-butt');
+      content.appendChild(preTitle);
+      content.appendChild(title);
+      content.appendChild(desc);
+      content.appendChild(button);
+    }
+
+    contentComp();
+    var roboPic = document.createElement('img');
+    roboPic.setAttribute('src', _robo.default);
+    illustration.appendChild(roboPic);
+  }
+
+  slideOneComp();
+
+  function slideTwoComp() {
+    var slideTwo = document.createElement('li');
+    slideTwo.setAttribute('class', 'glide__slide row-sb-c');
+    slidesCont.appendChild(slideTwo);
+    var content = document.createElement('div'),
+        illustration = document.createElement('div');
+    content.setAttribute('class', 'content');
+    illustration.setAttribute('class', 'illustration');
+    slideTwo.appendChild(content);
+    slideTwo.appendChild(illustration);
+
+    function contentComp() {
+      var preTitle = document.createElement('h6'),
+          title = document.createElement('h1'),
+          desc = document.createElement('p'),
+          desc2 = document.createElement('p');
+      preTitle.textContent = "Our Featured Artist";
+      title.textContent = "About Bonny Ive";
+      desc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet\n                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam illo perferendis. Molestias, saepe?";
+      desc2.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo enim voluptatibus totam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam!";
+      content.appendChild(preTitle);
+      content.appendChild(title);
+      content.appendChild(desc);
+      content.appendChild(desc2);
+    }
+
+    contentComp();
+    var roboPic = document.createElement('img');
+    roboPic.setAttribute('src', _robo2.default);
+    illustration.appendChild(roboPic);
+  }
+
+  slideTwoComp();
+
+  function slideThreeComp() {
+    var slideThree = document.createElement('li');
+    slideThree.setAttribute('class', 'glide__slide row-sb-c');
+    slidesCont.appendChild(slideThree);
+    var content = document.createElement('div'),
+        illustration = document.createElement('div');
+    content.setAttribute('class', 'content');
+    illustration.setAttribute('class', 'illustration');
+    slideThree.appendChild(content);
+    slideThree.appendChild(illustration);
+
+    function contentComp() {
+      var preTitle = document.createElement('h6'),
+          title = document.createElement('h1'),
+          desc = document.createElement('p'),
+          button = document.createElement('button');
+      preTitle.textContent = "Plan Your Visit";
+      title.textContent = "Buy Tickets Now";
+      desc.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet\n                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam\n                                illo perferendis. Molestias, saepe?";
+      button.textContent = "Buy Tickets";
+      button.setAttribute('class', 'feat-butt');
+      content.appendChild(preTitle);
+      content.appendChild(title);
+      content.appendChild(desc), content.appendChild(button);
+    }
+
+    contentComp();
+    var roboPic = document.createElement('img');
+    roboPic.setAttribute('src', _robo3.default);
+    illustration.appendChild(roboPic);
+  }
+
+  slideThreeComp();
+
+  function sliderControlsComp() {
+    var slideNavCont = document.createElement('div'),
+        prev = document.createElement('button'),
+        next = document.createElement('button');
+    slideNavCont.setAttribute('class', 'glide__arrows');
+    slideNavCont.setAttribute('data-glide-el', 'controls');
+    prev.setAttribute('class', 'glide__arrow glide__arrow--left prev');
+    prev.setAttribute('data-glide-dir', '<');
+    prev.textContent = "prev";
+    next.setAttribute('class', 'glide__arrow glide__arrow--right next');
+    next.setAttribute('data-glide-dir', '>');
+    next.textContent = "next";
+    slideNavCont.appendChild(prev);
+    slideNavCont.appendChild(next);
+    sliderCont.appendChild(slideNavCont);
+  }
+
+  sliderControlsComp(); // create logo text
+  //     const logo = document.createElement('div');
+  //     logo.setAttribute(`class`, 'logo');
+  //     logo.textContent = "moSpo";
+  //     // append the logo to its element
+  //     header.appendChild(logo);
+}
+
+mainComp();
+
+function footerComp() {
+  var footer = document.querySelector('footer'); // app.setAttribute(`id`, 'swup');
+  // create slider container
+
+  var footerCont = document.createElement('div');
+  footerCont.setAttribute('class', '');
+  var credit = document.createElement('p'),
+      me = document.createElement('a');
+  credit.textContent = 'moSpo is a fictional entity created within concept.';
+  me.textContent = 'Built by Tyrelle Phillips.';
+  me.setAttribute('href', 'https://tyrelle.dev');
+  me.setAttribute('target', '_blank');
+  footerCont.appendChild(credit);
+  footerCont.appendChild(me);
+  footer.appendChild(footerCont);
+}
+
+footerComp();
+new _glide.default('.glide').mount();
+},{"@glidejs/glide":"../node_modules/@glidejs/glide/dist/glide.esm.js","./img/robo1.jpg":"img/robo1.jpg","./img/robo2.jpg":"img/robo2.jpg","./img/robo3.jpg":"img/robo3.jpg","aos":"../node_modules/aos/dist/aos.js","aos/dist/aos.css":"../node_modules/aos/dist/aos.css"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
-import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide';
+import robotOne from './img/robo1.jpg';
+import robotTwo from './img/robo2.jpg';
+import robotThree from './img/robo3.jpg';
 
-new Glide('.glide').mount();
+
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -15,14 +18,245 @@ AOS.init({
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 0, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: true, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+// const footer = document.querySelector('footer');
+
+function headerComp() {
+    // create header alement, assign flexbox class
+    const header = document.querySelector('header');
+    header.setAttribute(`class`, 'row-sb-c');
+
+    // create logo text
+    const logo = document.createElement('div');
+    logo.setAttribute(`class`, 'logo');
+    logo.textContent = "moSpo";
+    // append the logo to its element
+    header.appendChild(logo);
+
+    // create the main menu element
+    function mainNavComp() {
+        const mainNav = document.createElement('nav');
+        header.appendChild(mainNav);
+        // assign main link elements and add to the main nav
+        const homeLink = document.createElement('a'),
+            galleryLink = document.createElement('a'),
+            museumLink = document.createElement('a'),
+            ticketsLink = document.createElement('a');
+        homeLink.textContent = "Home";
+        mainNav.appendChild(homeLink);
+        galleryLink.textContent = "Gallery";
+        mainNav.appendChild(galleryLink);
+        museumLink.textContent = "Museum";
+        mainNav.appendChild(museumLink);
+        ticketsLink.textContent = "Buy Tickets";
+        mainNav.appendChild(ticketsLink);
+    }
+    mainNavComp();
+
+    // create the social media nav element
+    function socNavComp() {
+        const socNav = document.createElement('nav');
+        socNav.setAttribute('class', 'row-fe');
+        header.appendChild(socNav);
+        // assign main link elements and add to the main nav
+        const dribLink = document.createElement('a'),
+            twitLink = document.createElement('a'),
+            instLink = document.createElement('a');
+        dribLink.setAttribute('class', 'fab fa-dribbble')
+        socNav.appendChild(dribLink);
+        twitLink.setAttribute('class', 'fab fa-twitter')
+        socNav.appendChild(twitLink);
+        instLink.setAttribute('class', 'fab fa-instagram')
+        socNav.appendChild(instLink);
+    }
+    socNavComp();
+
+};
+headerComp();
+
+function mainComp() {
+    const app = document.querySelector('main');
+    app.setAttribute(`id`, 'swup');
+    // app.setAttribute(`data-aos`, 'fade-up');
+
+    // create slider container
+    const sliderCont = document.createElement('div');
+    sliderCont.setAttribute('class', 'glide');
+    app.appendChild(sliderCont);
+    // create the secondary wrapper for the slider
+    const slideTrack = document.createElement('div');
+    slideTrack.setAttribute('class', 'glide__track');
+    slideTrack.setAttribute('data-glide-el', 'track');
+    sliderCont.appendChild(slideTrack);
+    // create the slider slides wrapper
+    const slidesCont = document.createElement('ul');
+    slidesCont.setAttribute('class', 'glide__slides');
+    slideTrack.appendChild(slidesCont);
+
+    function slideOneComp() {
+        const slideOne = document.createElement('li');
+        slideOne.setAttribute('class', 'glide__slide row-sb-c');
+        slidesCont.appendChild(slideOne);
+
+        const content = document.createElement('div'),
+            illustration = document.createElement('div');
+        content.setAttribute('class', 'content');
+        illustration.setAttribute('class', 'illustration');
+        slideOne.appendChild(content);
+        slideOne.appendChild(illustration);
+
+        function contentComp() {
+            const preTitle = document.createElement('h6'),
+                title = document.createElement('h1'),
+                desc = document.createElement('p'),
+                button = document.createElement('button');
+            preTitle.textContent = `February 6 - March 12`;
+            title.textContent = `Ive's Robo Exhibit`;
+            desc.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet
+                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam illo perferendis. Molestias, saepe?`;
+            button.textContent = "Learn More";
+            button.setAttribute('class', 'feat-butt');
+            content.appendChild(preTitle);
+            content.appendChild(title);
+            content.appendChild(desc);
+            content.appendChild(button);
+        }
+        contentComp();
+
+        const roboPic = document.createElement('img');
+        roboPic.setAttribute('src', robotOne);
+        illustration.appendChild(roboPic);
+
+    }
+    slideOneComp();
+
+    function slideTwoComp() {
+        const slideTwo = document.createElement('li');
+        slideTwo.setAttribute('class', 'glide__slide row-sb-c');
+        slidesCont.appendChild(slideTwo);
+
+        const content = document.createElement('div'),
+            illustration = document.createElement('div');
+        content.setAttribute('class', 'content');
+        illustration.setAttribute('class', 'illustration');
+        slideTwo.appendChild(content);
+        slideTwo.appendChild(illustration);
+
+        function contentComp() {
+            const preTitle = document.createElement('h6'),
+                title = document.createElement('h1'),
+                desc = document.createElement('p'),
+                desc2 = document.createElement('p');
+            preTitle.textContent = `Our Featured Artist`;
+            title.textContent = `About Bonny Ive`;
+            desc.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet
+                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam illo perferendis. Molestias, saepe?`;
+            desc2.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo enim voluptatibus totam! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam!`;
+            content.appendChild(preTitle);
+            content.appendChild(title);
+            content.appendChild(desc);
+            content.appendChild(desc2);
+        }
+        contentComp();
+
+        const roboPic = document.createElement('img');
+        roboPic.setAttribute('src', robotTwo);
+        illustration.appendChild(roboPic);
+
+    }
+    slideTwoComp();
+
+    function slideThreeComp() {
+        const slideThree = document.createElement('li');
+        slideThree.setAttribute('class', 'glide__slide row-sb-c');
+        slidesCont.appendChild(slideThree);
+
+        const content = document.createElement('div'),
+            illustration = document.createElement('div');
+        content.setAttribute('class', 'content');
+        illustration.setAttribute('class', 'illustration');
+        slideThree.appendChild(content);
+        slideThree.appendChild(illustration);
+
+        function contentComp() {
+            const preTitle = document.createElement('h6'),
+                title = document.createElement('h1'),
+                desc = document.createElement('p'),
+                button = document.createElement('button');
+
+            preTitle.textContent = `Plan Your Visit`;
+            title.textContent = `Buy Tickets Now`;
+            desc.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Non omnis tempore at amet
+                                doloribus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quibusdam
+                                illo perferendis. Molestias, saepe?`;
+            button.textContent = "Buy Tickets";
+            button.setAttribute('class', 'feat-butt');
+            content.appendChild(preTitle);
+            content.appendChild(title);
+            content.appendChild(desc), content.appendChild(button);
+
+        }
+        contentComp();
+
+        const roboPic = document.createElement('img');
+        roboPic.setAttribute('src', robotThree);
+        illustration.appendChild(roboPic);
+
+    }
+    slideThreeComp();
+
+    function sliderControlsComp() {
+        const slideNavCont = document.createElement('div'),
+            prev = document.createElement('button'),
+            next = document.createElement('button');
+        slideNavCont.setAttribute('class', 'glide__arrows');
+        slideNavCont.setAttribute('data-glide-el', 'controls');
+
+        prev.setAttribute('class', 'glide__arrow glide__arrow--left prev');
+        prev.setAttribute('data-glide-dir', '<');
+        prev.textContent = "prev";
+        next.setAttribute('class', 'glide__arrow glide__arrow--right next');
+        next.setAttribute('data-glide-dir', '>');
+        next.textContent = "next";
+        slideNavCont.appendChild(prev);
+        slideNavCont.appendChild(next);
+        sliderCont.appendChild(slideNavCont);
+    }
+    sliderControlsComp();
+    // create logo text
+    //     const logo = document.createElement('div');
+    //     logo.setAttribute(`class`, 'logo');
+    //     logo.textContent = "moSpo";
+    //     // append the logo to its element
+    //     header.appendChild(logo);
+}
+mainComp();
+
+function footerComp() {
+    const footer = document.querySelector('footer');
+    // app.setAttribute(`id`, 'swup');
+
+    // create slider container
+    const footerCont = document.createElement('div');
+    footerCont.setAttribute('class', '');
+    const credit = document.createElement('p'),
+        me = document.createElement('a');
+    credit.textContent = 'moSpo is a fictional entity created within concept.';
+    me.textContent = 'Built by Tyrelle Phillips.'
+    me.setAttribute('href', 'https://tyrelle.dev');
+    me.setAttribute('target', '_blank');
+    footerCont.appendChild(credit);
+    footerCont.appendChild(me);
+    footer.appendChild(footerCont);
+}
+footerComp();
+new Glide('.glide').mount();
